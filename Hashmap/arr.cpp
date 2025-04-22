5e2d298d6aa2faaf0936099bac5728177c6f12fc
shashwat283 <shashwatrajwanshi41@gmail.com>
2025-04-22 09:09:24 +0530

    completed recursion

---
+++ Hashmap/arr.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]= {1,2,3,5,2,43,23,34,5,23,2,34,2,3,56,135,132,45};
+    const int size = sizeof(arr)/sizeof(arr[0]);
+    int countarr[size] = {0};
+    for(int i =0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;  
+    int count = 0;
+    for(int i =0;i<size;i++){
+        if
+    }
+    cout<<endl;
+    return 0;
+}
